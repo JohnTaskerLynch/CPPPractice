e7c8080159ec1ff56262f8f46ddad2c41ee11c3f
JohnTaskerLynch <Tasker_Lynch-J@ulster.ac.uk>
2023-02-25 13:46:37 +0000

    bogobogo

---
+++ cmake-build-debug/CMakeFiles/Bogosort.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "Bogosort.exe"
+  "Bogosort.exe.manifest"
+  "Bogosort.pdb"
+  "CMakeFiles/Bogosort.dir/Sort.cpp.obj"
+  "CMakeFiles/Bogosort.dir/Sort.cpp.obj.d"
+  "CMakeFiles/Bogosort.dir/main.cpp.obj"
+  "CMakeFiles/Bogosort.dir/main.cpp.obj.d"
+  "libBogosort.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Bogosort.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
