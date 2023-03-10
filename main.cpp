2298a67804fcaf86a3bdfe83e95d3375635f6f2d
JohnTaskerLynch <Tasker_Lynch-J@ulster.ac.uk>
2023-03-10 21:09:30 +0000

    desktop

--- main.cpp
+++ main.cpp
@@ -7,45 +7,59 @@
 
 using namespace std;
 
-const int SCREEN_WIDTH = 950, SCREEN_HEIGHT = 500;
+// window attributes
+const int SCREEN_WIDTH = 940, SCREEN_HEIGHT = 500, FPS_LIMIT = 1;
 
+// input array
+const vector<int> TEST_ARRAY = {1, 4, 3, 2, 6, 5, 8, 7, 9, 10, 12, 11};
+
+// -- enter program --
 int main(void) {
-    // input array
-    vector<int> testArray = {1, 4, 3, 2, 6, 5, 8, 7};
+    // location of x-axis
+    int xAxis = 475;
 
     // pass counter
-    long counter;
+    long counter = 0;
 
     // initialise window
-    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Bogo");
-    SetTargetFPS(250);
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Bogosort Visualization");
+    SetTargetFPS(FPS_LIMIT);
 
-    // main loop
+    // -- main loop --
     while (!WindowShouldClose()) {
-        // variable loop
-        vector<int> pass = sortBogo(testArray, counter);
-        float offset = 100;
+        // -- variable loop --
 
-        // drawing
+        // create pass array every frame
+        vector<int> pass = sortBogo(TEST_ARRAY, counter);
+
+        // TODO: make offset based on no. of bars
+        float offset = SCREEN_WIDTH/TEST_ARRAY.size();
+
+        // -- end variable loop --
+
+        // -- drawing --
         BeginDrawing();
-        DrawLine(0, SCREEN_HEIGHT - 400, SCREEN_WIDTH, SCREEN_HEIGHT - 400, WHITE);
 
-        // sort
-        for(int i = 0; i < testArray.size(); i++) {
+        // x-axis line
+        DrawLine(0, xAxis, SCREEN_WIDTH, xAxis, WHITE);
+
+        // iterate through each pass value
+        for(int i = 0; i < TEST_ARRAY.size(); i++) {
             // print current value to terminal
-            cout << pass.at(i);
+            cout << pass.at(i) << " ";
 
             // draw a bar relative to the height of the current value
-            DrawRectangle(offset, SCREEN_HEIGHT - (400 + pass.at(i) * 10), 35, pass.at(i) * 10, WHITE);
+            DrawRectangle(offset, (xAxis - (pass.at(i)*10)), 35, pass.at(i) * 10, WHITE);
 
             // TODO: show passes counter on screen
+
 
             // bar x pos offset increment
             offset += 100;
         }
 
         // newline for each pass
-        cout << endl;
+        cout << "- pass " << counter << endl;
 
         // close screen if bars get into order
         if(checkOrder(pass)) {
@@ -55,11 +69,13 @@ int main(void) {
         // clear current frame
         ClearBackground(BLACK);
 
-        // end drawing
+        // -- end drawing --
         EndDrawing();
+
+        // -- exit main loop --
     }
 
-    // exit program
+    // -- exit program --
     CloseWindow();
     return 0;
 }
